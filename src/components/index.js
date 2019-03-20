682cfba155ad83e442f5fe4a1c59916d8dc97da4
joaaoeu <hello@joaao.eu>
2019-03-20 20:39:33 -0300

    Improved structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
+import App from './App';
 import Header from './Header';
 import Post from './Post';
 import PostHeader from './PostHeader';
 
-export { Header, Post, PostHeader };
+export {
+  App, Header, Post, PostHeader,
+};
--- src/index.js
+++ src/index.js
@@ -1,66 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
 // Styles
-import './styles/main.scss';
-import './style.scss';
+import './styles/app.scss';
 
 // Components
-import Header from './components/Header';
-import Post from './components/Post';
-
-class App extends Component {
-  state = {
-    posts: [
-      {
-        id: 3,
-        author: {
-          profileImage: 'https://avatars1.githubusercontent.com/u/2254731?s=400&v=4',
-          name: 'Diego Fernandes',
-        },
-        timestamp: 'há 4 min',
-        content:
-          'Amet consectetur adipisicing elit. Eaque earum blanditiis qui quibusdam dolores quaerat rem accusantium aperiam libero. Unde asperiores repellendus dolor minima assumenda obcaecati natus iure atque enim.',
-      },
-      {
-        id: 2,
-        author: {
-          profileImage: 'https://avatars2.githubusercontent.com/u/4248081?s=400&v=4',
-          name: 'Filipe Deschamps',
-        },
-        timestamp: 'há 8 min',
-        content:
-          'Ducimus velit quasi quis delectus ipsam amet quod, ipsum similique a necessitatibus labore assumenda quo quas? Doloremque repellat esse alias praesentium dicta natus modi, accusantium, qui obcaecati mollitia ab molestias.',
-      },
-      {
-        id: 1,
-        author: {
-          profileImage: 'https://avatars3.githubusercontent.com/u/6677405?s=400&v=4',
-          name: 'João Vitor',
-        },
-        timestamp: 'há 10 min',
-        content:
-          'Hic, praesentium repudiandae debitis natus incidunt sint! Numquam nisi accusamus odio? Totam ipsum modi praesentium repellat. Vero dolore doloremque vitae distinctio assumenda non incidunt, expedita harum ratione consequuntur! Reiciendis, dolorum?',
-      },
-    ],
-  };
-
-  renderPosts = () => {
-    const { posts } = this.state;
-
-    const allPosts = posts.map(post => <Post postData={post} key={post.id} />);
-
-    return <div className="posts-content">{allPosts}</div>;
-  };
-
-  render() {
-    return (
-      <Fragment>
-        <Header title="Rocketbook" />
-        {this.renderPosts()}
-      </Fragment>
-    );
-  }
-}
+import App from './components/App';
 
 render(<App />, document.getElementById('app'));
